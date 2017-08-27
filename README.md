d5291276c12be65968a19e7ae409a28c50e3df23
atanvarno <atanvarno69@gmail.com>
2017-08-27 21:09:21 +0100

    v0.1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pls\Http\Client
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](https://github.com/PHP-library-standards/http-client/blob/master/LICENSE)
+[![Latest Version](https://img.shields.io/github/release/PHP-library-standards/http-client.svg?style=flat-square)](https://github.com/PHP-library-standards/http-client/releases)
---
+++ src/Client.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package   Pls\Http\Client
+ * @author    PHP Library Standards <https://github.com/PHP-library-standards>
+ * @copyright 2017 PHP Library Standards
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+
+namespace Pls\Http\Client;
+
+use Pls\Http\Message\{Request, Response};
+
+interface Client
+{
+    public function sendRequest(Request $request): Response;
+}
